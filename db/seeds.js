6ba2968edf0b59428ae5032910d8fb061a38514e
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-07-26 12:58:55 -0700

    searchbar backend is completeed

--- db/seeds.js
+++ db/seeds.js
@@ -5,9 +5,9 @@ const Car = require('../model/Car')
 
 const cars = [
     {   
-        image: 'skefnwf',
+        image: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         price: 13500,
-        title: "Clean",
+        title: "Audi",
         location: "Denver, Co",
         year: 2015,
         mileage: 120000,
@@ -17,7 +17,7 @@ const cars = [
         ownerInfo: '322E'
     },
     {   
-        image: 'dgrjaegp',
+        image: 'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         price: 27000,
         title: "Clean",
         location: "LA",
