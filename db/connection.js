6a19764864b4572caaaa43039ae9384b700b323e
sashaharmer@yahoo.com <sashaharmer@yahoo.com>
2022-08-08 15:52:18 -0400

    test

--- db/connection.js
+++ db/connection.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 
 
-const mongoURI = 'mongodb://127.0.0.1:27017/car-shop'
+// const mongoURI = 'mongodb://127.0.0.1:27017/car-shop'
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true
-})
-.then(instance => {
-    console.log(`Connected on ${instance.connections[0].name }`)
-})
-.catch(err => console.log(`Error in connection, ${err}`))
-
-
-// mongoose.connect(process.env.DATABASE_URL,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true
 // })
-
-// const db = mongoose.connection
-
-// db.on('connected', () => {
-//     console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+// .then(instance => {
+//     console.log(`Connected on ${instance.connections[0].name }`)
 // })
+// .catch(err => console.log(`Error in connection, ${err}`))
+
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('connected', () => {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+})
